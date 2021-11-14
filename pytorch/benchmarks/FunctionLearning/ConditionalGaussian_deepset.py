8b0d385295af4069c793dcec33ba87dd80c4e967
Kaze Wong <kazewong.physics@gmail.com>
2021-11-14 13:53:59 -0500

    Move models into pytorch

--- pytorch/benchmarks/FunctionLearning/ConditionalGaussian_deepset.py
+++ pytorch/benchmarks/FunctionLearning/ConditionalGaussian_deepset.py
@@ -14,8 +14,8 @@ np.random.seed(100)
 def Gaussian(x,mean,sigma):
 	return np.log(1./(sigma*np.sqrt(2*np.pi))*np.exp(-1/2*((x-mean)/sigma)**2))
 
-N_hyper = 1000
-N_samples = 100
+N_hyper = 100
+N_samples = 1000
 
 x_axis = np.linspace(-5,5,N_samples)
 hyper_samples = np.random.uniform(size=(N_hyper,2))
@@ -30,8 +30,8 @@ for i in range(N_hyper):
 	hyper_x.append(np.repeat(hyper_samples[i:i+1],N_samples,axis=0))
 	y.append(Gaussian(x_axis,hyper_samples[i,0],hyper_samples[i,1])[:,None])
 
-n_sample_points = 5
-n_hyper_points = 5
+n_sample_points = 3
+n_hyper_points = 3
 
 train_x = x[:int(N_hyper*0.8)]
 
@@ -75,11 +75,11 @@ def query_tree_array(samples, hyper, sample_tree_array, hyper_tree, n_sample_poi
     return output,hyper_query
 
 train_index = query_tree_array(train_x,train_hyper_x, sample_tree_array, hyper_tree, n_sample_points,n_hyper_points,training_mode=True)
-train_index = (cum_length[train_index[1]][...,None].repeat(5,axis=2)+train_index[0]).reshape(-1,n_hyper_points*n_sample_points).astype(int)
+train_index = (cum_length[train_index[1]][...,None].repeat(n_sample_points,axis=2)+train_index[0]).reshape(-1,n_hyper_points*n_sample_points).astype(int)
 
 
 valid_index = query_tree_array(valid_x,valid_hyper_x, sample_tree_array, hyper_tree, n_sample_points,n_hyper_points,training_mode=True)
-valid_index = (cum_length[valid_index[1]][...,None].repeat(5,axis=2)+valid_index[0]).reshape(-1,n_hyper_points*n_sample_points).astype(int)
+valid_index = (cum_length[valid_index[1]][...,None].repeat(n_sample_points,axis=2)+valid_index[0]).reshape(-1,n_hyper_points*n_sample_points).astype(int)
 
 
 train_x = torch.cat((torch.from_numpy(train_x),torch.from_numpy(train_hyper_x)),dim=1).cuda().float()
@@ -91,10 +91,11 @@ valid_context = train_x[valid_index]
 valid_y = torch.from_numpy(valid_y).cuda().float()
 
 
-model = DeepSets_Siren_Weight(in_features=3, out_features=1, hidden_features=128, hidden_layers=3,set_features=50)
+model = DeepSets_Siren_Weight(in_features=3, out_features=1, hidden_features=128, hidden_layers=3,set_features=128)
 model.train().cuda().float()
 optimizer = torch.optim.Adam(model.parameters(),lr=1e-2)
 
+print('Start training')
 
 batch_size = int(2e4+1)
 valid_loss = 1e10
@@ -104,14 +105,16 @@ for epoch in range(30000):
     np.random.shuffle(indices)
     running_loss = 0.0
     for index in range(indices.shape[0]//batch_size+1):
-        locations = (train_context-train_x[:,None])[index*batch_size:(index+1)*batch_size]
+        locations = train_context[index*batch_size:(index+1)*batch_size]
         values = train_y[train_index][index*batch_size:(index+1)*batch_size]
+        query = train_x[index*batch_size:(index+1)*batch_size]
         outputs = train_y[index*batch_size:(index+1)*batch_size]
         optimizer.zero_grad()
 
-        pred_y = model(locations,values)
+        pred_y = model(locations,values,query)
 #        pred_y = model(inputs)
         loss = torch.mean(torch.sum(((pred_y-outputs)/(outputs))**2,dim=1))
+#        loss = torch.mean(torch.sum(((pred_y-outputs))**2,dim=1))
         loss.backward()
     #    nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0, norm_type=2)
         optimizer.step()
@@ -119,9 +122,10 @@ for epoch in range(30000):
 
     if epoch%1==0:
         with torch.no_grad():
-            pred_y = model(valid_context-valid_x[:,None],train_y[valid_index])
+            pred_y = model(valid_context,train_y[valid_index],valid_x)
             #pred_y = model(valid_x)
             valid_loss_temp = torch.mean(torch.sum(((pred_y-valid_y)/valid_y)**2,dim=1))
+#            valid_loss_temp = torch.mean(torch.sum(((pred_y-valid_y))**2,dim=1))
             if valid_loss_temp<=valid_loss:
                 print("saving")
                 print("Training loss is :"+str(running_loss))
