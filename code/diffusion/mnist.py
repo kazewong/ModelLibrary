595381190b053af2e5bc0425279a5e9a76f7cbce
Kaze Wong <kazewong.physics@gmail.com>
2023-07-17 20:27:40 -0400

    update mnist

--- code/diffusion/mnist.py
+++ code/diffusion/mnist.py
@@ -2,6 +2,10 @@ from sde_score import ScordBasedSDE
 from common.Unet import Unet
 import jax
 import jax.numpy as jnp
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torchvision.datasets import MNIST
+import tqdm
 
 seed = 0
 
@@ -9,3 +13,36 @@ unet = Unet(2, [1,4,8,16], jax.random.PRNGKey(seed))
 sde = ScordBasedSDE(unet, lambda x: 1.0, lambda x: 1.0, lambda x: 1.0)
 
 test_data = jnp.zeros((1, 32, 32))
+
+BATCH_SIZE = 64
+LEARNING_RATE = 3e-4
+STEPS = 300
+PRINT_EVERY = 30
+SEED = 5678
+
+key = jax.random.PRNGKey(SEED)
+
+normalise_data = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5,), (0.5,)),
+    ]
+)
+train_dataset = torchvision.datasets.MNIST(
+    "MNIST",
+    train=True,
+    download=True,
+    transform=normalise_data,
+)
+test_dataset = torchvision.datasets.MNIST(
+    "MNIST",
+    train=False,
+    download=True,
+    transform=normalise_data,
+)
+trainloader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=BATCH_SIZE, shuffle=True
+)
+testloader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=BATCH_SIZE, shuffle=True
+)
