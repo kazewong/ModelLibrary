2da2a85ab7be9c12fef07557ca5d779e8cdfb8d0
Kaze Wong <kazewong.physics@gmail.com>
2023-07-22 21:42:15 -0400

    initilalize vision transformer

---
+++ jax/common/VisionTransformer.py
@@ -0,0 +1,14 @@
+import jax
+import jax.numpy as jnp
+import equinox as eqx
+from jaxtyping import Array, PRNGKeyArray
+from typing import Callable
+
+class VIT(eqx.Module):
+
+    def __init__(self,
+                )
+        pass
+
+    def __call__(self, x: Array) -> Array:
+        pass
