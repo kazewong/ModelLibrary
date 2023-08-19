78c2795d90d06ae3cd7d70b5399cc2c307009749
Kaze Wong <kazewong.physics@gmail.com>
2023-08-19 19:35:55 -0400

    Up Down of Unet run now

--- src/kazeML/jax/common/Unet.py
+++ src/kazeML/jax/common/Unet.py
@@ -105,6 +105,8 @@ class UnetBlock(eqx.Module):
 @dataclass
 class UnetConfig:
     num_dim: int
+    input_channels: int
+    output_channels: int
     embedding_dim: int
     base_channels: int = 4
     n_resolution: int = 4
@@ -123,6 +125,8 @@ class UnetConfig:
 
 
 class Unet(eqx.Module):
+    input_conv: eqx.nn.Conv
+    output_conv: eqx.nn.Conv
     DownBlocks: list[ResnetBlock]
     UpBlocks: list[ResnetBlock]
     BottleNeck: list[ResnetBlock]
@@ -154,6 +158,22 @@ class Unet(eqx.Module):
             fir_kernel_size=config.fir_kernel_size,
         )
 
+        key, subkey = jax.random.split(key)
+        self.input_conv = eqx.nn.Conv(
+            config.num_dim,
+            config.input_channels,
+            config.base_channels,
+            kernel_size=1,
+            key=subkey,
+        )
+        self.output_conv = eqx.nn.Conv(
+            config.num_dim,
+            config.base_channels,
+            config.output_channels,
+            kernel_size=1,
+            key=subkey,
+        )
+
         for i_level in range(config.n_resolution):
             for i_block in range(config.n_resnet_blocks):
                 key, subkey = jax.random.split(key)
@@ -180,8 +200,10 @@ class Unet(eqx.Module):
             self.BottleNeck.append(
                 ResBlock(
                     key=subkey,
-                    num_in_channels=config.base_channels * 2 ** (config.n_resolution),
-                    num_out_channels=config.base_channels * 2 ** (config.n_resolution),
+                    num_in_channels=config.base_channels
+                    * 2 ** (config.n_resolution - 1),
+                    num_out_channels=config.base_channels
+                    * 2 ** (config.n_resolution - 1),
                 )
             )
 
@@ -191,7 +213,7 @@ class Unet(eqx.Module):
                 self.UpBlocks.append(
                     ResBlock(
                         key=subkey,
-                        num_in_channels=config.base_channels * 2 ** (i_level + 1),
+                        num_in_channels=config.base_channels * 2**i_level,
                         num_out_channels=config.base_channels * 2**i_level,
                     )
                 )
@@ -213,6 +235,8 @@ class Unet(eqx.Module):
         key: PRNGKeyArray,
         train: bool = True,
     ) -> Array:
+        key, subkey = jax.random.split(key)
+        x = self.input_conv(x)
         for block in self.DownBlocks:
             key, subkey = jax.random.split(key)
             x = block(x, subkey, t, train=train)
@@ -222,4 +246,5 @@ class Unet(eqx.Module):
         for block in self.UpBlocks:
             key, subkey = jax.random.split(key)
             x = block(x, subkey, t, train=train)
+        x = self.output_conv(x)
         return x
