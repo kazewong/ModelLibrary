fd6ecc54e304f9b7bc18a60cec48ee81b99ea6c2
Kaze Wong <kazewong.physics@gmail.com>
2023-08-29 10:36:15 -0400

    Add series feature extractor

--- src/kazeML/jax/data2vec/data2vec_dataset.py
+++ src/kazeML/jax/data2vec/data2vec_dataset.py
@@ -1,8 +1,9 @@
 from torch.utils.data import Dataset
 import h5py
+from jaxtyping import Array, Float
 
 class Data2VecDataset(Dataset):
-    def __init__(self, path: str, transform=None):
+    def __init__(self, path: str, transform=None, n_example: int = 1):
         h5_file = h5py.File(path, "r")
         self.transform = transform
 
@@ -11,16 +12,19 @@ class Data2VecDataset(Dataset):
         assert isinstance(self.data, h5py.Dataset), "Data is not a dataset"
 
         self.n_dim = len(self.data.shape[2:])
+        self.n_example = 
 
     def __len__(self):
         return len(self.data)
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> tuple[Float[Array, "batch"], Float[Array, "batch n_example"]]:
         sample = self.data[index]
         if self.transform != None:
             for f in self.transform:
                 sample = f(sample)
+        
         return sample
 
-    def get_shape(self):
-        return self[0].shape
+    @staticmethod
+    def make_mask(n_mask: int) -> Array:
+        raise NotImplementedError
--- src/kazeML/jax/data2vec/feature_extractor.py
+++ src/kazeML/jax/data2vec/feature_extractor.py
@@ -1,44 +1,98 @@
 from abc import ABC, abstractmethod
-from jaxtyping import Array
+from typing import Callable
+from jaxtyping import Array, PRNGKeyArray
+import jax
+import jax.numpy as jnp
+import equinox as eqx
 
-class FeatureExtractor(ABC):
 
+class FeatureExtractor(eqx.Module):
     @abstractmethod
     def __init__(self):
         pass
 
     @abstractmethod
-    def extract_features(self,
-                         data: Array) -> Array:
+    def extract_features(self, data: Array) -> Array:
         """
-        
+
         The first dimension should be number of patches and the second dimension should be size of embedding.
         """
         pass
 
     @abstractmethod
-    def embed(self,
-            data: Array,) -> Array:
+    def embed(
+        self,
+        data: Array,
+    ) -> Array:
         pass
+
 
 class ImageFeatureExtractor(FeatureExtractor):
-
     def __init__(self):
         super().__init__()
 
     def extract_features(self, data):
         pass
+
 
 class SeriesFeatureExtractor(FeatureExtractor):
-    
-    def __init__(self):
+
+    layer: list[eqx.nn.Sequential]
+    log_compression: bool
+    skip_connections: bool
+    residual_connections: float = 1.0
+
+    def __init__(
+        self,
+        key: PRNGKeyArray,
+        num_channels: int,
+        num_layers: int,
+        kernel_size: int = 3,
+        stride: int = 1,
+        p_dropout: float = 0.0,
+        affine_group_norm: bool = False,
+        log_compression: bool = False,
+        skip_connections: bool = False,
+        residual_connections: float = 1.0,
+        activation: Callable = jax.nn.gelu,
+    ):
         super().__init__()
+
+        self.layer = []
+        for i in range(num_layers):
+            key, subkey = jax.random.split(key)
+            self.layer.append(
+                eqx.nn.Sequential(
+                    [
+                        eqx.nn.Conv1d(
+                            num_channels,
+                            num_channels,
+                            kernel_size,
+                            stride,
+                            use_bias=False,
+                            key=subkey
+                        ),
+                        eqx.nn.Dropout(p_dropout),
+                        eqx.nn.GroupNorm(
+                            1,
+                            num_channels,
+                            affine=affine_group_norm,
+                        ),
+                        eqx.nn.Lambda(activation)
+                    ]
+                )
+            )
+
+        self.log_compression = log_compression
+        self.skip_connections = skip_connections
+        self.residual_connections = residual_connections
+
 
     def extract_features(self, data):
         pass
 
-class TextFeatureExtractor(FeatureExtractor):
 
+class TextFeatureExtractor(FeatureExtractor):
     def __init__(self):
         super().__init__()
 
