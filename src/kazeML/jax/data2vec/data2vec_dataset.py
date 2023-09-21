453939497685134204a0e3d0e9c32a4f0f068d73
Kaze Wong <kazewong.physics@gmail.com>
2023-09-21 17:29:35 -0400

    Black formatting

--- src/kazeML/jax/data2vec/data2vec_dataset.py
+++ src/kazeML/jax/data2vec/data2vec_dataset.py
@@ -6,6 +6,7 @@ from jaxtyping import Array, Float
 import numpy as np
 from kazeML.jax.data2vec.feature_extractor import FeatureExtractor
 
+
 class Data2VecDataset(Dataset):
     def __init__(
         self,
@@ -66,7 +67,11 @@ class Data2VecDataset(Dataset):
                 0, self.data_length - mask_length, (num_mask,)
             )
             for i in range(num_mask):
-                mask[center_indices[i]-mask_length//2:center_indices[i]+mask_length//2] = 1
+                mask[
+                    center_indices[i]
+                    - mask_length // 2 : center_indices[i]
+                    + mask_length // 2
+                ] = 1
         elif self.n_dim == 2:
             data_length = int(np.sqrt(self.data_length))
             mask_length = int(np.sqrt(self.mask_length) * data_length)
@@ -74,11 +79,17 @@ class Data2VecDataset(Dataset):
             num_mask = int(self.mask_fraction / self.mask_length + torch.rand(1))
             num_mask = max(num_mask, self.min_masks)
             center_indices = torch.randint(
-                mask_length//2, data_length - mask_length//2, (num_mask, 2)
+                mask_length // 2, data_length - mask_length // 2, (num_mask, 2)
             )
             for i in range(num_mask):
-                mask[center_indices[i, 0]-mask_length//2:center_indices[i, 0]+mask_length//2,
-                     center_indices[i, 1]-mask_length//2:center_indices[i, 1]+mask_length//2] = 1
+                mask[
+                    center_indices[i, 0]
+                    - mask_length // 2 : center_indices[i, 0]
+                    + mask_length // 2,
+                    center_indices[i, 1]
+                    - mask_length // 2 : center_indices[i, 1]
+                    + mask_length // 2,
+                ] = 1
             mask = mask.reshape(-1)
         elif self.n_dim == 3:
             data_length = int(np.cbrt(self.data_length))
@@ -87,21 +98,31 @@ class Data2VecDataset(Dataset):
             num_mask = int(self.mask_fraction / self.mask_length + torch.rand(1))
             num_mask = max(num_mask, self.min_masks)
             center_indices = torch.randint(
-                mask_length//2, data_length - mask_length//2, (num_mask, 3)
+                mask_length // 2, data_length - mask_length // 2, (num_mask, 3)
             )
             for i in range(num_mask):
-                mask[center_indices[i, 0]-mask_length//2:center_indices[i, 0]+mask_length//2,
-                     center_indices[i, 1]-mask_length//2:center_indices[i, 1]+mask_length//2,
-                     center_indices[i, 2]-mask_length//2:center_indices[i, 2]+mask_length//2] = 1
+                mask[
+                    center_indices[i, 0]
+                    - mask_length // 2 : center_indices[i, 0]
+                    + mask_length // 2,
+                    center_indices[i, 1]
+                    - mask_length // 2 : center_indices[i, 1]
+                    + mask_length // 2,
+                    center_indices[i, 2]
+                    - mask_length // 2 : center_indices[i, 2]
+                    + mask_length // 2,
+                ] = 1
             mask = mask.reshape(-1)
         else:
             print("Dimension not supported")
             raise NotImplementedError
 
         return mask
-    
+
     def set_data_length(self, feature_extractor: FeatureExtractor):
-        self.data_length = feature_extractor.extract_features(jnp.array(self.data[0])).shape[-1]
+        self.data_length = feature_extractor.extract_features(
+            jnp.array(self.data[0])
+        ).shape[-1]
 
     @property
     def data_shape(self):
