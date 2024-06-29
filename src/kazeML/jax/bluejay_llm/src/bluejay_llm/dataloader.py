7bad436fcc202bcf5bd6756b4bf788568d0026bb
Kaze Wong <kazewong.physics@gmail.com>
2024-06-29 16:43:22 -0400

    Refactor ThePileDataset class to use numpy and torch tensors

--- src/kazeML/jax/bluejay_llm/src/bluejay_llm/dataloader.py
+++ src/kazeML/jax/bluejay_llm/src/bluejay_llm/dataloader.py
@@ -1,19 +1,14 @@
 from torch.utils.data import Dataset
 import torch
-import torch.nn.functional as F
-import tiktoken
-import json
-
+import numpy as np
 class ThePileDataset(Dataset):
-    def __init__(self, path: str, tokenizer: tiktoken.Encoding, max_length: int = 1024):
-        file = open(path, "r").readlines()
-        self.tokenizer = tokenizer
+    def __init__(self, path: str, max_length: int = 1024):
         self.max_length = max_length
-        self.data = list(map(lambda x: self.tokenizer.encode(json.loads(x)['text']),file))
-        # self.data = torch.stack(list(map(lambda x: F.pad(torch.tensor(x), pad=(0, max_length-len(x)), value=self.tokenizer.eot_token), data)))
+        data = np.memmap(path, dtype=np.int16, mode="r")
+        self.data = torch.tensor(data, dtype=torch.int16)
 
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, index: int) -> torch.Tensor:
-        return torch.tensor(self.data[index])
+        return self.data[index]
