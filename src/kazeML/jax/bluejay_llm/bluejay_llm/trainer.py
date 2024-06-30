0b24cca10fe012b25692cd796fdfd25667e2411c
Kaze Wong <kazewong.physics@gmail.com>
2024-06-30 13:28:56 -0400

    Update trainer initialization

--- src/kazeML/jax/bluejay_llm/bluejay_llm/trainer.py
+++ src/kazeML/jax/bluejay_llm/bluejay_llm/trainer.py
@@ -1,18 +1,22 @@
 from bluejay_llm.bluejay import GPT
 from bluejay_llm.dataloader import ThePileDataset
 
+from jaxtyping import PRNGKeyArray, PyTree, Float, Array
 import jax
 import jax.numpy as jnp
 import optax
-import equinox
+import equinox as eqx
 import numpy as np
 import wandb
 import matplotlib.pyplot as plt
 from tap import Tap
 
 import torch
+from torch.utils.data import DataLoader, random_split
+from torch.utils.data.distributed import DistributedSampler
 
 from typing import Literal
+
 
 class BlueJayExperimentParser(Tap):
 
@@ -33,9 +37,11 @@ class BlueJayExperimentParser(Tap):
     prefetch_factor: int = 2
 
     # Logging hyperparameters
+    logging: bool = True
     log_epoch: int = 2
     log_t_step: int = 10
     output_path: str = "./experiment"
+
 
 class BlueJayModelParser(Tap):
     # Model hyperparameters
@@ -46,15 +52,106 @@ class BlueJayModelParser(Tap):
     dropout: float = 0.0
     bias: bool = True
 
+
 class BigParser(BlueJayExperimentParser, BlueJayModelParser):
     pass
 
+
 class BlueJayTrainer:
 
-    def __init__(self,
+    def __init__(self, dataset: ThePileDataset, config: BigParser) -> None:
+        self.config = config
+        if self.config.logging and jax.process_index() == 0:
+            wandb.init(
+                project=self.config.project_name,
+                name=self.config.experiment_name,
+                config=config.as_dict(),
+            )
 
-                 ) -> None:
-        self.test = 1
+        n_processes = jax.process_count()
+        devices = np.array(jax.devices())
+        self.global_mesh = jax.sharding.Mesh(devices, ("batch"))
+        self.sharding = jax.sharding.NamedSharding(
+            self.global_mesh,
+            jax.sharding.PartitionSpec(
+                ("batch"),
+            ),
+        )
+
+        train_set, test_set = random_split(
+            dataset, [config.train_test_ratio, 1 - config.train_test_ratio]
+        )
+
+        train_sampler = DistributedSampler(
+            train_set,
+            num_replicas=n_processes,
+            rank=jax.process_index(),
+            shuffle=True,
+            seed=config.seed,
+        )
+        test_sampler = DistributedSampler(
+            test_set,
+            num_replicas=n_processes,
+            rank=jax.process_index(),
+            shuffle=False,
+            seed=config.seed,
+        )
+        self.train_loader = DataLoader(
+            train_set,
+            batch_size=config.batch_size,
+            num_workers=config.num_workers,
+            sampler=train_sampler,
+            prefetch_factor=config.prefetch_factor,
+            pin_memory=False,
+        )
+        self.test_loader = DataLoader(
+            test_set,
+            batch_size=config.batch_size,
+            num_workers=config.num_workers,
+            sampler=test_sampler,
+            prefetch_factor=config.prefetch_factor,
+            pin_memory=False,
+        )
+
+        self.key, subkey = jax.random.split(jax.random.PRNGKey(config.seed))
+
+        self.model = GPT(
+            config.vocab_size,
+            config.block_size,
+            config.n_layer,
+            config.n_embd,
+            config.dropout,
+            config.bias,
+            key=subkey,
+        )
+        learning_rate = config.learning_rate
+        self.optimizer = optax.chain(
+            optax.adamw(learning_rate), optax.clip_by_global_norm(1.0)
+        )
+        self.opt_state = self.optimizer.init(eqx.filter(self.model, eqx.is_array))
 
     def train(self):
-        print(self.test)
+        pass
+
+    @staticmethod
+    def run_step(
+        model: GPT,
+        opt_state: PyTree,
+        batch: Float[Array, "batch 1 datashape"],
+        key: PRNGKeyArray,
+        opt_update,
+        train: bool = True,
+    ):
+        pass
+
+    def run_epoch(
+        self,
+        model: GPT,
+        opt_state: PyTree,
+        data_loader: DataLoader,
+        key: PRNGKeyArray,
+        epoch: int,
+        log_loss: bool = False,
+        train: bool = True,
+    ) -> tuple[GPT, PyTree, Array | float]:
+        return model, opt_state, 0.0
